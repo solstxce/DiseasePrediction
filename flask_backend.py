1d879c3f2c7a42dc37277407a5a785961f6fed8c
solstxce <48374053+solstxce@users.noreply.github.com>
2024-10-19 21:38:22 +0530

    updated more

--- flask_backend.py
+++ flask_backend.py
@@ -112,7 +112,8 @@ if __name__ == '__main__':
         sample_users = [
             {'email': 'admin@example.com', 'password': 'admin123', 'role': 'admin'},
             {'email': 'doctor@example.com', 'password': 'doctor123', 'role': 'doctor'},
-            {'email': 'user@example.com', 'password': 'user123', 'role': 'user'}
+            {'email': 'user@example.com', 'password': 'user123', 'role': 'user'},
+            {'email': 'admin@gmail.com.com', 'password': 'admin123', 'role': 'admin'}
         ]
         users_collection.insert_many(sample_users)
 
